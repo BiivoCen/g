ff91f33e91b9c51b15fb82451f1d1db2dce3a5e1
chb <cenxiaoxiaoman@gmail.com>
2019-11-18 16:36:14 +0800

    lombok jwt 拦截器

---
+++ src/main/java/com/testgit/g/util/GetToken.java
@@ -0,0 +1,13 @@
+package com.testgit.g.util;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+public class GetToken {
+    public String getToken(User user) {
+        String token = "";
+        token = JWT.create().withAudience(user.getId())
+                .sign(Algorithm.HMAC256(user.getPassword()));
+        return token;
+    }
+}
---
+++ src/main/java/com/testgit/g/util/UserApi.java
@@ -0,0 +1,4 @@
+package com.testgit.g.util;
+
+public class UserApi {
+}
